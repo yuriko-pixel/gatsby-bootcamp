cf3d57d8ee409c93f193f18a8a4c770c4f459a25
Yuriko Darragh <beljar731@gmail.com>
2020-05-20 20:12:49 +0900

    updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -46,6 +46,12 @@ const Contact = ()=> {
        subject,
        message
       };
+      
+      const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
 
     fetch("/", {
       method: "POST",
