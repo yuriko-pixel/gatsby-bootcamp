f14f7ee66e81b5afbb5bc76885b18bf6f6019dee
yuriko-pixel <55519118+yuriko-pixel@users.noreply.github.com>
2020-05-24 14:21:33 +0900

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -37,7 +37,13 @@ const MyForm = () => {
          </div>), form);
         })
         .catch(r => {
-          handleServerResponse(false, r.response.data.error, form);
+          handleServerResponse(false, (<div className={conModule.messagecon}>
+           <div className={conModule.success}>
+             <i className="fa fa-check-circle success"></i>
+           </div>
+             <span className={conModule.sucmessage}>Message sent successfully</span>
+             <span className={conModule.sucmessage}>I will reply as soon as possible.</span>
+         </div>), form);
         });
     };
     return (
